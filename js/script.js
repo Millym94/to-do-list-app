5521292495bc98087f161cb744963b7508309982
Millym94 <Millym94@gmail.com>
2023-12-22 21:34:14 -0500

    Update script.js
    used jquery to create the to-do-app.

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function newItem() {
+    // 1. Adding a new item to the list of items 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = $('#list');
+        list.append(li);
+        $('#input').val(""); // Reset input field
+    }
+
+    // 2. Crossing out an item from the list of items:
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', crossOut);
+
+    // 3(i). Adding the delete Button "X":
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
+
+    // 3(ii). Adding class delete (display: none) from the css:
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+
+    // 4. Recording the items:
+    $('#list').sortable();
+}
+
+
+function newItemOnEnter(event) {
+    var x = event.code;
+    if(x == "Enter") {
+        event.preventDefault(); // Don't submit form
+        newItem();
+    }    
+}
